a5743eabc38230be06afeeaa8216b7130bf3d2b7
Martin Häger <martin.hager@minmb.se>
2013-04-14 18:51:31 +0200

    class.banlist.php: these methods should be declared static (strict standards)

--- include/class.banlist.php
+++ include/class.banlist.php
@@ -16,17 +16,17 @@
 
 class Banlist {
     
-    function add($email,$submitter='') {
+    static function add($email,$submitter='') {
         $sql='INSERT IGNORE INTO '.BANLIST_TABLE.' SET added=NOW(),email='.db_input($email).',submitter='.db_input($submitter);
         return (db_query($sql) && ($id=db_insert_id()))?$id:0;
     }
     
-    function remove($email) {
+    static function remove($email) {
         $sql='DELETE FROM '.BANLIST_TABLE.' WHERE email='.db_input($email);
         return (db_query($sql) && db_affected_rows())?true:false;
     }
     
-    function isbanned($email) {
+    static function isbanned($email) {
         return db_num_rows(db_query('SELECT id FROM '.BANLIST_TABLE.' WHERE email='.db_input($email)))?true:false;
     }
 }
