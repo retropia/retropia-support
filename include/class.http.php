0aff8fb3ad1b963cbe2082e31e13c55b57a8693c
Martin Häger <martin.hager@minmb.se>
2013-04-14 18:58:39 +0200

    class.http.php: these methods should be declared static (strict standards)

--- include/class.http.php
+++ include/class.http.php
@@ -15,7 +15,7 @@
 **********************************************************************/
 class Http {
     
-    function header_code_verbose($code) {
+    static function header_code_verbose($code) {
         switch($code):
         case 200: return '200 OK';
         case 204: return '204 NoContent';
@@ -27,10 +27,10 @@ class Http {
         endswitch;
     }
     
-    function response($code,$content,$contentType='text/html',$charset='UTF-8') {
+    static function response($code,$content,$contentType='text/html',$charset='UTF-8') {
 		
-        header('HTTP/1.1 '.Http::header_code_verbose($code));
-		header('Status: '.Http::header_code_verbose($code)."\r\n");
+        header('HTTP/1.1 '.self::header_code_verbose($code));
+		header('Status: '.self::header_code_verbose($code)."\r\n");
 		header("Connection: Close\r\n");
 		header("Content-Type: $contentType; charset=$charset\r\n");
         header('Content-Length: '.strlen($content)."\r\n\r\n");
@@ -38,7 +38,7 @@ class Http {
         exit;
     }
 	
-	function redirect($url,$delay=0,$msg='') {
+	static function redirect($url,$delay=0,$msg='') {
 
         if(strstr($_SERVER['SERVER_SOFTWARE'], 'IIS')){
             header("Refresh: $delay; URL=$url");
